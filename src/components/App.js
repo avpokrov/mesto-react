927169da4272a21ede806ca4f845a40457b650d8
Tedr <avpokrov@gmail.com>
2025-04-26 09:35:54 +0300

    feat: Начало проекта, создание чистого реакт

---
+++ src/components/App.js
@@ -0,0 +1,112 @@
+import logo from '../images/logo.svg';
+
+function App() {
+  return (
+   
+    <body class="page">
+      <main class="main center">
+        <header class="header ident-top40px">
+          <img src={logo} alt="logo" class="header__logo"/>
+        </header>
+        <section class="profile ident-top40px">
+          <div class="profile_box">
+            <div class="profile__wrapper">
+              <img class="profile__image" src="" alt="avatar"/>
+            </div>
+            <div class="profile__info">
+              <div class="profile__name-box">
+                <h1 class="profile__name"></h1>
+                <button class="button button_editProfile active-element"></button>
+              </div>
+              <p class="profile__description"></p>
+            </div>
+          </div>
+          <button class="button button_addCard active-element"></button>
+        </section>
+        <ul class="cards ident-top40px">
+        </ul>
+        <footer class="footer">
+          <p class="footer__text">&#169; 2024 Mesto Russia</p>
+        </footer>
+      </main>
+      <section class="popup popup-editProfile">
+        <div class="popup__container">
+          <button class="button button_close active-element"></button>
+          <h2 class="popup__header">Редактировать профиль</h2>
+          <form name="editProfile" class="popup__form" novalidate>
+            <input id="nameProfile" name="name" class="popup__input popup__field-text popup-name" minlength="2"
+              maxlength="40" required></input>
+            <span class="nameProfile-error popup__field-text-error"></span>
+            <input id="descriptionProfile" name="about" class="popup__input popup__field-text popup-description"
+              minlength="2" maxlength="200" required></input>
+            <span class="descriptionProfile-error popup__field-text-error"></span>
+            <button class="popup__button active-element">Сохранить</button>
+          </form>
+        </div>
+      </section>
+      <section class="popup popup-createCard">
+        <div class="popup__container">
+          <button class="button button_close active-element"></button>
+          <h2 class="popup__header">Новое место</h2>
+          <form name="addCard" class="popup__form" novalidate>
+            <input id="addCard" name="name" class="popup__input popup__field-text popup-name" placeholder="Название"
+              minlength="2" maxlength="30" required></input>
+            <span class="addCard-error popup__field-text-error"></span>
+            <input id="urlCard" name="link" class="popup__input popup__field-text popup-description"
+              placeholder="Введите url" type="url"></input>
+            <span class="urlCard-error popup__field-text-error"></span>
+            <button class="popup__button active-element">Создать</button>
+          </form>
+        </div>
+      </section>
+      <section class="popup popup-img">
+        <div class="popup-img__container">
+          <button class="button button_close active-element"></button>
+          <img class="popup-img__image" src="" alt=""/>
+          <h3 class="popup-img__text"></h3>
+        </div>
+      </section>
+    
+      <section class="popup popup-accept">
+        <div class="popup__container">
+          <button class="button button_close active-element"></button>
+          <h2 class="popup__header">Вы уверены?</h2>
+          <div class="popup-accept__block">
+            <button class="popup__button active-element popup__button_accept popup__button_accept_accept">Да</button>
+            <button class="popup__button active-element popup__button_accept popup__button_accept_cancel">Нет</button>
+          </div>
+        </div>
+      </section>
+      <section class="popup popup-updateAvatar">
+        <div class="popup__container">
+          <button class="button button_close active-element"></button>
+          <h2 class="popup__header">Обновить аватар</h2>
+          <form name="updateAvatar" class="popup__form" novalidate>
+            <input id="urlAvatar" name="link" class="popup__input popup__field-text popup-link" placeholder="Введите url"
+            pattern="https?://.+" type="url" required></input>
+            <span class="urlAvatar-error popup__field-text-error"></span>
+            <button class="popup__button active-element">Сохранить</button>
+          </form>
+        </div>
+      </section>
+    
+      <template id="card">
+        <li class="card">
+          <div class="trash active-element"></div>
+          <img class="card__img" src="" alt=""/>
+          <div class="card__info">
+            <h2 class="card__name"></h2>
+            <div class="like__block">
+              <div class="like active-element"></div>
+              <div class="like__sum"></div>
+            </div>
+          </div>
+        </li>
+      </template>
+    </body>
+    
+
+  );
+}
+
+export default App;
